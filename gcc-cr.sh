2de3f4d102d0e66520d10f1f9900ba6b4956593c
PazPacifica <Rueslin.Rana@outlook.com>
2025-03-04 20:03:59 +0200

    added argv support

--- gcc-cr.sh
+++ gcc-cr.sh
@@ -1,17 +1,48 @@
-
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <filename.c> [libraries...]"
+LIBRARIES=""
+PARAMETERS=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -l)
+            shift
+            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+                LIBRARIES+=" -l$1"
+                shift
+            done
+            ;;
+        -p)
+            shift
+            while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+                PARAMETERS+=" $1"
+                shift
+            done
+            ;;
+        *)
+            if [[ -z "$C_FILE" ]]; then
+                C_FILE="$1"
+            else
+                echo "Unknown option or too many arguments: $1"
+                exit 1
+            fi
+            shift
+            ;;
+    esac
+done
+
+if [[ -z "$C_FILE" ]]; then
+    echo "Usage: $0 <filename.c> [-l library1 library2 ...] [-p param1 param2 ...]"
     exit 1
 fi
 
-BASENAME=$(basename "$1" .c)
+BASENAME=$(basename "$C_FILE" .c)
 
-gcc -o "$BASENAME.o" "$1" "${@:2}"
+gcc -o "$BASENAME.o" "$C_FILE" $LIBRARIES
 
-if [ $? -eq 0 ]; then
-    ./"$BASENAME.o"
+
+if [[ $? -eq 0 ]]; then
+    ./"$BASENAME" $PARAMETERS
 else
     echo "Compilation failed."
     exit 1
